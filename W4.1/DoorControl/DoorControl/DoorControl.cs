2166fe9685aa283d6c8d5d0edc653967f1186d16
Jacob Gade <jacobcgade@gmail.com>
2022-02-23 16:44:25 +0100

    SWT week 4 DoorControl exercise. I am at part 2 atm.

---
+++ W4.1/DoorControl/DoorControl/DoorControl.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoorControl
+{
+    internal class DoorControl : IDoorControl
+    {
+        public enum DoorState
+        {
+            DoorClosed,
+            DoorClosing,
+            DoorOpening,
+            DoorBreached
+        };
+
+        private IDoor _door;
+        private IEntryNotification _en;
+        private IAlarm _alarm;
+        private IUserValidation _uv;
+        private DoorState _state;
+
+        public DoorControl(IDoor door, IEntryNotification en, IAlarm alarm, IUserValidation uv)
+        {
+            _state = DoorState.DoorClosed;
+            _door = door;
+            _en = en;
+            _alarm = alarm;
+            _uv = uv;
+        }
+
+        public void DoorOpened()
+        {
+            _door.Close();
+            _state = DoorState.DoorClosing;
+        }
+
+        public void DoorClosed()
+        {
+            _state = DoorState.DoorClosed;
+        }
+
+        public void RequestEntry(int id)
+        {
+            if (_uv.ValidateEntryRequest(id))
+            {
+                _door.Open();
+                _en.NotifyEntryGranted(id);
+                _state = DoorState.DoorOpening;
+            }
+
+        }
+    }
+}
---
+++ W4.1/DoorControl/DoorControl/Interfaces.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoorControl
+{
+    public interface IDoor
+    {
+        void Open();
+        void Close();
+    }
+
+    public interface IDoorControl
+    {
+        void DoorOpened();
+        void DoorClosed();
+        void RequestEntry(int id);
+    }
+
+    public interface IEntryNotification
+    {
+        void NotifyEntryGranted(int id);
+        void NotifyEntryDenied(int id);
+    }
+
+    public interface IAlarm
+    {
+        void RaiseAlarm();
+    }
+
+    public interface IUserValidation
+    {
+        bool ValidateEntryRequest(int id);
+    }
+
+}
